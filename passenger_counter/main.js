b23ae1f9fb8dc4e796f76891109deaf03694cfb8
Shashidhar Biradar <shashidharb459@gmail.com>
2023-11-02 00:54:03 +0530

    First Commit

---
+++ passenger_counter/main.js
@@ -0,0 +1,18 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+let count = 0;
+
+function increment(){
+    count++;
+    countEl.innerText=count;
+}
+
+let msg = "";
+function save()
+{
+    countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0
+    countEl.textContent = 0
+}
